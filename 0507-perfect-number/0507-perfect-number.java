cfb01b3ef40f96015600dc47f1c9f9a983c8ad98
Tanishq Tomar <tanishqtomar07@gmail.com>
2026-07-24 10:58:37 +0530

    Time: 1050 ms (63.74%), Space: 41.9 MB (80.54%) - LeetHub

---
+++ 0507-perfect-number/0507-perfect-number.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean checkPerfectNumber(int num) {
+        int sum = 0;
+        int n = num/2;
+        for(int i = 1;i<=n;i++){
+            if(num%i == 0) sum += i;
+        }
+        return (sum == num);
+        
+    }
+}

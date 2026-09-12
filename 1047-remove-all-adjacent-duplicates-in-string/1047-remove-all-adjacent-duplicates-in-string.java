bcbd19f0de75105d29719765b9f25b20545be2f4
Tanishq Tomar <tanishqtomar07@gmail.com>
2026-09-12 15:52:11 +0530

    Time: 131 ms (5.56%), Space: 49.1 MB (5.59%) - LeetHub

--- 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.java
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.java
@@ -7,23 +7,10 @@ class Solution {
 
             else stk.push(c);
         }
-        int i = 0;
-        char[] chars = new char[stk.size()];
-        while(!stk.isEmpty())
-        { 
-            chars[i++] = stk.pop();
-        }
-
-        int left = 0, right = chars.length - 1;
-        while (left < right) {
-            char temp = chars[left];
-            chars[left] = chars[right];
-            chars[right] = temp;
-            left++;
-            right--;
-        }
         String res = "";
-        for(char c : chars) res += c;
+        while(!stk.isEmpty()){
+            res = stk.pop()+res;
+        }
 
         return res;
     }

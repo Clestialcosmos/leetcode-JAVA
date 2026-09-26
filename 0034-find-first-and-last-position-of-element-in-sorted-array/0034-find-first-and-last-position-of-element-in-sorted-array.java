ec58362430b5d30131dec84b9e11d611402b3b70
Tanishq Tomar <tanishqtomar07@gmail.com>
2026-09-26 19:15:34 +0530

    Time: 0 ms (100%), Space: 48.1 MB (71.08%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,37 +1,28 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-            // Find start
-         int first = -1, last = -1;
-        int start = 0, end = nums.length - 1, mid;
-
-        // Find first occurrence
-        while (start <= end) {
-            mid = start+(end-start) / 2;
-            if (nums[mid] == target) {
-                first = mid;
-                end = mid - 1; 
-            } else if (nums[mid] < target) {
-                start = mid + 1;
-            } else {
-                end = mid - 1;
+        int[] freq = {-1,-1};
+        int start = 0,end = nums.length-1;
+        while(start <= end){
+            int mid = start + (end - start)/2;
+            if(nums[mid]== target){
+                freq[0] = mid;
+                end = mid-1;
             }
+            else if(nums[mid] > target) end = mid-1;
+            else start = mid+1;
         }
 
-        // Find last occurrence
-        start = 0; end = nums.length - 1;
-        while (start <= end) {
-            mid = start +(end-start) / 2;
-            if (nums[mid] == target) {
-                last = mid;
-                start = mid + 1; 
-            } else if (nums[mid] < target) {
-                start = mid + 1;
-            } else {
-                end = mid - 1;
+        start = 0;
+        end = nums.length-1;
+        while(start <= end){
+            int mid = start + (end-start)/2;
+            if(nums[mid] == target){
+                freq[1] = mid;
+                start = mid+1;
             }
+            else if(nums[mid] > target) end = mid-1;
+            else start = mid+1;
         }
-
-        return new int[]{first,last};
-        // Time complexity = O(log N)    
+        return freq;
     }
 }
